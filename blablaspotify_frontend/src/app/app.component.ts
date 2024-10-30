3a2aa39848db1c3b105a7f461717fc390c61b61c
Daniel Battaglia <daniel.battaglia@mailbox.org>
2024-10-30 21:19:57 +0100

    Cleanup app component and toggler/tumbler

--- blablaspotify_frontend/src/app/app.component.ts
+++ blablaspotify_frontend/src/app/app.component.ts
@@ -1,13 +1,7 @@
 import {Component, computed, inject} from '@angular/core';
-import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
-import {MatGridList, MatGridTile} from '@angular/material/grid-list';
 import {MatToolbar} from '@angular/material/toolbar';
-import {RouterOutlet} from '@angular/router';
-import {BlablaComponent} from './blabla/blabla.component';
 import {PlayerComponent} from './blabla/player/player.component';
-import {StationSearchComponent} from './blabla/station-search/station-search.component';
 import {MusicComponent} from './music/music.component';
-import {TogglerComponent} from './toggler/toggler.component';
 import { ControlsComponent } from "./controls/controls.component";
 import { ControlsService } from './controls/controls.service';
 
@@ -15,10 +9,10 @@ import { ControlsService } from './controls/controls.service';
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, MatToolbar, PlayerComponent, MatGridList, MatGridTile, MatCard, MatCardHeader, MatCardTitle,
-    MatCardContent,
-    StationSearchComponent, BlablaComponent, MusicComponent, TogglerComponent,
-    ControlsComponent
+    MatToolbar,
+    PlayerComponent,
+    MusicComponent,
+    ControlsComponent,
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
