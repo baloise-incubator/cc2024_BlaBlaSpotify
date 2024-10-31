5486c5996fe4bb19133737ad4222fdaee8379c8a
Daniel Battaglia <daniel.battaglia@mailbox.org>
2024-10-31 10:13:38 +0100

    Remove play button from music player and control playback fully by service

--- blablaspotify_frontend/src/app/music/spotify.service.ts
+++ blablaspotify_frontend/src/app/music/spotify.service.ts
@@ -20,6 +20,9 @@ export class SpotifyService {
   avatarUrl: string = '';
   player: any;
   playlists: Playlist[] = [];
+  currentPlayListUrn?: string;
+  nextPlayListUrn?: string;
+  isActive = false;
 
   constructor(private httpClient: HttpClient) {
     this.init()
@@ -33,7 +36,6 @@ export class SpotifyService {
           this.isAuthorized = true;
           this.initAvatarUrl();
           this.initPlaylists();
-          console.log("");
         }
       },
       error: (error) => {
@@ -57,23 +59,32 @@ export class SpotifyService {
     });
   }
 
+  changePlayListUrn(playListUrn: string) {
+    this.nextPlayListUrn = playListUrn
+
+    if (this.isActive && this.nextPlayListUrn !== this.currentPlayListUrn) {
+      this.play(this.nextPlayListUrn)
+    }
+  }
+
   play(urn?: string | undefined) {
-    if (!urn && this.player) {
+    this.isActive = true;
+
+    if (!urn && this.player && this.currentPlayListUrn !== undefined) {
       this.player.resume()
+      return
     } else {
-      this.httpClient.get('/api/spotify/play?uri=' + urn).subscribe({
-        next: (data: any) => {
-          console.log('playlist started');
-        },
-        error: (error) => {
-          console.error(error);
-        }
+      this.currentPlayListUrn = urn ? urn : this.nextPlayListUrn
+      this.httpClient.get('/api/spotify/play?uri=' + this.currentPlayListUrn).subscribe({
+        next: (data: any) => console.log('playlist started'),
+        error: (error) => console.error(error)
       });
     }
   }
 
   pause() {
-    this.player.pause()
+    this.isActive = false;
+    this.player?.pause()
   }
 
   initPlayer() {
@@ -87,7 +98,7 @@ export class SpotifyService {
     this.player.connect();
   }
 
-  initPlaylists() {
+  private initPlaylists() {
     this.httpClient.get('/api/spotify/playlists').subscribe({
       next: (data: any) => {
         this.playlists = (data.items as []).map((playlist: any) => {
@@ -97,6 +108,7 @@ export class SpotifyService {
           p.imageUrl = playlist.images?.[0].url ?? '';
           return p;
         });
+        this.nextPlayListUrn = this.playlists[0].urn
       },
       error: (error) => {
         console.error(error);
